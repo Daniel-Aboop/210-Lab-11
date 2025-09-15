f6db2cf671a969cfeab5c02e3d47545ffcc5ede5
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-09-15 14:09:00 -0700

    2:08 pm

--- Lab11.cpp
+++ Lab11.cpp
@@ -1,9 +1,11 @@
-// COMSC-210 | Lab 5 | Daniel Santisteban
+// COMSC-210 | Lab 11 | Daniel Santisteban
 // IDE used: VS Code
 
 #include <iostream>
 using namespace std;
 const int size=4,psize=3;
+
+
 
 struct Computer{
     public:
@@ -15,6 +17,8 @@ struct Computer{
     programs=nullptr;
 }
 };
+
+//
 void inputComputerData(Computer *roster){
     static int computerNum=1;
     cout<<"Computer Data for Computer #"<<computerNum<<endl;
@@ -24,21 +28,23 @@ void inputComputerData(Computer *roster){
     cout<<"Enter Ram amount:";
     cin>>roster->ram;
     cout<<endl;
-    cout<<"Enter program list:"<<endl;
+    cout<<"Enter Program list:"<<endl;
     roster->programs=new string[psize];
     cin.ignore();
     for(int i=0;i<psize;i++){
+        cout<<"Program #"<<i+1<<endl;
         getline(cin,roster->programs[i]);
     }
-    
+
 
     computerNum++;
 }
+
 void displayComputerData(Computer *roster){
     static int computerNum=1;
-    cout<<"Computer Data for #"<<computerNum<<endl;
-    cout<<"Operating System: "<<roster->operatingsys<<endl;
-    cout<<"Ram Amount: "<<roster->ram<<"gb"<<endl;
+    cout<<endl<<"Computer Data for #"<<computerNum<<endl<<endl;
+    cout<<"Operating System: "<<roster->operatingsys<<endl<<endl;;
+    cout<<"Ram Amount: "<<roster->ram<<"gb"<<endl<<endl;
     cout<<"Programs in system: ";
     for(int i=0;i<psize;i++){
         cout<<roster->programs[i]<<",";

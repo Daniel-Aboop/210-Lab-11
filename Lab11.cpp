e76ba1bf2f11345635228635ec7cdcc37cd11128
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-09-15 12:49:43 -0700

    10 mins

---
+++ Lab11.cpp
@@ -0,0 +1,27 @@
+// COMSC-210 | Lab 5 | Daniel Santisteban
+// IDE used: VS Code
+
+#include <iostream>
+using namespace std;
+
+const int size=4,computers=3;
+struct Computer{
+
+
+
+~Computer(){
+
+
+
+}
+
+};
+
+
+
+int main(){
+    Computer *roster = new Computer[size];
+    
+
+    return 0;
+}
